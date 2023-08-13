ba1a43ca5853f067965e0e4800603d89c5aff487
Viktor Pozdeev <gibgivs@mail.ru>
2023-08-13 16:32:39 +0300

    adding a condition for checking the end on 11

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main() {
     val likes = 10_002
 
-    if (likes % 10 == 1) {
+    if (likes % 10 == 1 && likes % 100 != 11) {
         println("Понравилось $likes человеку")
     } else {
         println("Понравилось $likes людям")
