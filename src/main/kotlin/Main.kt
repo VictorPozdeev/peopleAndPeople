5a81c328cc67ce09e2e9b36defa4d606e4760542
Viktor Pozdeev <gibgivs@mail.ru>
2023-08-13 15:25:28 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val likes = 10_002
+
+    if (likes % 10 == 1) {
+        println("Понравилось $likes человеку")
+    } else {
+        println("Понравилось $likes людям")
+    }
+}
